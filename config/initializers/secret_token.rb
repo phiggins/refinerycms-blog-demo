136308f876e0115577e45ed3d88ab444319e2e7b
Pete Higgins <pete@peterhiggins.org>
2012-04-24 20:46:27 -0700

    refinery 2.0.3 with blog.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefinerycmsBlogDemo::Application.config.secret_token = '6ad8a44060d5e22219a56eef40a8358040f08b56de4501a091065b34b5df56c1eb4a7cdd60bd4bc63714b88caaa1cd137a53fa7c59005ff8ed61ff6182fcdd10'
